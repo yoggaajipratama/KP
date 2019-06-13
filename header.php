5c3a4ce7f6fc0113c7bf623f3322bc52099e0429
yoggaajipratama <yoggaajipratama99@gmail.com>
2019-06-13 11:29:10 +0700

    2nd

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>StikerMania | Dashboard</title>
-  <link rel="stylesheet" href="text/css" href="kertas.css">
+  <link rel="stylesheet" type="text/css" href="glossy.css">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
