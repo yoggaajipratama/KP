829c62b69e63be2423ca20733cc3e07f5d661ff8
yoggaajipratama <yoggaajipratama99@gmail.com>
2019-06-21 23:16:01 +0700

    Insert

--- delete.php
+++ delete.php
@@ -1,9 +1,15 @@
 <?php include 'connection.php'; ?>
 <?php
 	$id = $_GET['id'];
-	$delete = "DELETE FROM `dt_stkr` WHERE `dt_stkr`.`id` = '$id'";
-	if(mysqli_query($sql, $delete)){
-	    header('Location: index.php');
+	$show = mysqli_query($sql, "SELECT image FROM dt_stkr WHERE id ='$id' ");
+	$data = mysqli_fetch_array($show);
+	$image = $data['image'];
+	unlink("images/".$image);
+	$delete = mysqli_query($sql, "DELETE FROM `dt_stkr` WHERE `dt_stkr`.`id` = '$id'");
+
+
+	if($delete){
+  		header('Location: index.php');
 	}else{
 	    echo "Hapus data gagal";
 	}
