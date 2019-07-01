8b67df2e1834be4bfb221be0507ec9b3adc4bf15
Iqbal Gumilar <iqbalgmlar@gmail.com>
2019-07-01 10:08:27 +0700

    finalisasi

--- db/connection.php
+++ db/connection.php
@@ -1,20 +1,19 @@
 <?php 
-	$sql = mysqli_connect("localhost", "root", "", "stiker");
+	$sql = mysqli_connect("localhost", "root", "123", "stiker");
 	if (!$sql) {
 		echo "Gagal";
 	}
 	$selesai = "Selesai";
 	$bselesai = "Belum Selesai";
-	$glossy = "Glossy";
-	$matte = "Matte";
+	$glossy = "glossy";
+	$matte = "matte";
 	
-	$jenis_kertas = mysqli_query($sql, "SELECT * FROM jenis_kertas");
-	$warna_kertas = mysqli_query($sql, "SELECT * FROM warna");
 	$warna_tinta = mysqli_query($sql, "SELECT * FROM sTinta");
+	$rugi = mysqli_query($sql, "SELECT * FROM kerugian");
 	$english_color_g = mysqli_query($sql, "SELECT * FROM english_color WHERE types='glossy'");
 	$english_color_m = mysqli_query($sql, "SELECT * FROM english_color WHERE types='matte'");
 	
-	$result = mysqli_query($sql, "SELECT * FROM dt_stkr WHERE status = '$bselesai' ORDER BY id DESC");
+	$result = mysqli_query($sql, "SELECT * FROM dt_stkr WHERE status = '$bselesai' ORDER BY tanggal DESC");
 	$kertasmatte = mysqli_query($sql, "SELECT * FROM sKertas WHERE jenis = '$matte' ORDER BY tgl DESC");
 	
 	$kertasglossy = mysqli_query($sql, "SELECT * FROM sKertas WHERE jenis = '$glossy' ORDER BY tgl DESC");
